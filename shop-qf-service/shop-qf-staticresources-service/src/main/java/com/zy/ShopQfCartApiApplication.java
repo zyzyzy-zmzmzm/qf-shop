0216dc7a06debe9764e0526a6d1536d9b572ee65
zy <zy195716@163.com>
2020-03-09 23:33:24 +0800

    ss

---
+++ shop-qf-service/shop-qf-staticresources-service/src/main/java/com/zy/ShopQfCartApiApplication.java
@@ -0,0 +1,13 @@
+package com.zy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopQfCartApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopQfCartApiApplication.class, args);
+    }
+
+}
